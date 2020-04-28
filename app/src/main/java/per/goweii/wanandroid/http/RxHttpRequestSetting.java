b8c3d535ee17ef4bca330c6a064f6a83821acd7e
goweii <goweii@163.com>
2020-04-28 21:05:31 +0800

    删除Ponyo库

--- app/src/main/java/per/goweii/wanandroid/http/RxHttpRequestSetting.java
+++ app/src/main/java/per/goweii/wanandroid/http/RxHttpRequestSetting.java
@@ -1,18 +1,13 @@
 package per.goweii.wanandroid.http;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.franmontiel.persistentcookiejar.PersistentCookieJar;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParser;
 
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import per.goweii.basic.core.common.Config;
-import per.goweii.basic.utils.LogUtils;
 import per.goweii.rxhttp.request.setting.DefaultRequestSetting;
 import per.goweii.rxhttp.request.utils.HttpsCompat;
 
@@ -51,25 +46,6 @@ public class RxHttpRequestSetting extends DefaultRequestSetting {
     @Override
     public int getSuccessCode() {
         return 0;
-    }
-
-    @Nullable
-    @Override
-    public Interceptor[] getNetworkInterceptors() {
-        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-            @Override
-            public void log(String message) {
-                String msg;
-                try {
-                    msg = mGson.toJson(JsonParser.parseString(message));
-                } catch (Exception e) {
-                    msg = message;
-                }
-                LogUtils.i("HttpLoggingInterceptor", msg);
-            }
-        });
-        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        return new Interceptor[]{httpLoggingInterceptor};
     }
 
     @Override
--- basic_utils/src/main/java/per/goweii/basic/utils/LogUtils.java
+++ basic_utils/src/main/java/per/goweii/basic/utils/LogUtils.java
@@ -2,7 +2,11 @@ package per.goweii.basic.utils;
 
 import android.util.Log;
 
-import per.goweii.ponyo.log.Ponlog;
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Arrays;
 
 /**
  * Log输出工具类
@@ -12,62 +16,133 @@ import per.goweii.ponyo.log.Ponlog;
  * @date 2018/3/18-上午10:21
  */
 public final class LogUtils {
-    private static boolean DEBUGGABLE = BuildConfig.DEBUG;
+    public static final int VERBOSE = 1;
+    public static final int DEBUG = 1 << 1;
+    public static final int INFO = 1 << 2;
+    public static final int WARN = 1 << 3;
+    public static final int ERROR = 1 << 4;
+    public static final int NONE = 0;
+    public static final int ALL = VERBOSE | DEBUG | INFO | WARN | ERROR;
+    public static final int DEBUG_ABOVE = DEBUG | INFO | WARN | ERROR;
+    public static final int INFO_ABOVE = INFO | WARN | ERROR;
+    public static final int WARN_ABOVE = WARN | ERROR;
 
-    private static Ponlog.Logger sLogger;
+    @Level
+    private static int FILTER = ALL;
 
-    static {
-        sLogger = Ponlog.INSTANCE.create();
-        sLogger.setInvokeClass(LogUtils.class);
+    public static void setFilter(@Level int filter) {
+        LogUtils.FILTER = filter;
+    }
+
+    public static void v(String tag, String msg) {
+        log(tag, msg, VERBOSE);
     }
 
     public static void v(String tag, Object msg) {
-        log(Log.VERBOSE, tag, msg);
+        log(tag, msg, VERBOSE);
+    }
+
+    public static void v(String tag, Object[] msg) {
+        v(tag, Arrays.toString(msg));
+    }
+
+    public static void d(String tag, String msg) {
+        log(tag, msg, DEBUG);
     }
 
     public static void d(String tag, Object msg) {
-        log(Log.DEBUG, tag, msg);
+        log(tag, msg, DEBUG);
+    }
+
+    public static void d(String tag, Object[] msg) {
+        d(tag, Arrays.toString(msg));
+    }
+
+    public static void i(String tag, String msg) {
+        log(tag, msg, INFO);
     }
 
     public static void i(String tag, Object msg) {
-        log(Log.INFO, tag, msg);
+        log(tag, msg, INFO);
+    }
+
+    public static void i(String tag, Object[] msg) {
+        i(tag, Arrays.toString(msg));
+    }
+
+    public static void w(String tag, String msg) {
+        log(tag, msg, WARN);
     }
 
     public static void w(String tag, Object msg) {
-        log(Log.WARN, tag, msg);
+        log(tag, msg, WARN);
+    }
+
+    public static void w(String tag, Object[] msg) {
+        w(tag, Arrays.toString(msg));
+    }
+
+    public static void e(String tag, String msg) {
+        log(tag, msg, ERROR);
     }
 
     public static void e(String tag, Object msg) {
-        log(Log.ERROR, tag, msg);
+        log(tag, msg, ERROR);
     }
 
-    public static void a(String tag, Object msg) {
-        log(Log.ASSERT, tag, msg);
+    public static void e(String tag, Object[] msg) {
+        e(tag, Arrays.toString(msg));
     }
 
-    private static void log(int level, String tag, Object msg) {
-        if (!DEBUGGABLE) return;
-        switch (level) {
-            case Log.VERBOSE:
-                sLogger.log(Ponlog.Level.VERBOSE, tag, msg);
+    public static void log(String tag, Object msg, int filter) {
+        if (msg instanceof char[]) {
+            log(tag, Arrays.toString((char[]) msg), filter);
+        } else if (msg instanceof byte[]) {
+            log(tag, Arrays.toString((byte[]) msg), filter);
+        } else if (msg instanceof short[]) {
+            log(tag, Arrays.toString((short[]) msg), filter);
+        } else if (msg instanceof int[]) {
+            log(tag, Arrays.toString((int[]) msg), filter);
+        } else if (msg instanceof long[]) {
+            log(tag, Arrays.toString((long[]) msg), filter);
+        } else if (msg instanceof float[]) {
+            log(tag, Arrays.toString((float[]) msg), filter);
+        } else if (msg instanceof double[]) {
+            log(tag, Arrays.toString((double[]) msg), filter);
+        } else if (msg instanceof boolean[]) {
+            log(tag, Arrays.toString((boolean[]) msg), filter);
+        } else {
+            log(tag, String.valueOf(msg), filter);
+        }
+    }
+
+    private static void log(String tag, String msg, int filter) {
+        if (!BuildConfig.DEBUG) {
+            return;
+        }
+        switch (FILTER & filter) {
+            case VERBOSE:
+                Log.v(tag, msg);
                 break;
-            case Log.DEBUG:
-                sLogger.log(Ponlog.Level.DEBUG, tag, msg);
+            case DEBUG:
+                Log.d(tag, msg);
                 break;
-            case Log.INFO:
-                sLogger.log(Ponlog.Level.INFO, tag, msg);
+            case INFO:
+                Log.i(tag, msg);
                 break;
-            case Log.WARN:
-                sLogger.log(Ponlog.Level.WARN, tag, msg);
+            case WARN:
+                Log.w(tag, msg);
                 break;
-            case Log.ERROR:
-                sLogger.log(Ponlog.Level.ERROR, tag, msg);
-                break;
-            case Log.ASSERT:
-                sLogger.log(Ponlog.Level.ASSERT, tag, msg);
+            case ERROR:
+                Log.e(tag, msg);
                 break;
             default:
                 break;
         }
+    }
+
+    @IntDef({VERBOSE, DEBUG, INFO, WARN, ERROR, NONE, ALL, DEBUG_ABOVE, INFO_ABOVE, WARN_ABOVE})
+    @Retention(RetentionPolicy.SOURCE)
+    @interface Level {
     }
 }
